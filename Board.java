a16221ebac6ee68ffe3156e8dff4d78e06c93bf6
datafridge <datafridge@posteo.de>
2017-04-27 22:50:36 +0200

    basic construction of board

---
+++ Board.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * Board stores all configurations of the current room
+ *
+ * @author Leon Hansen, Felix Schmidt
+ * @version 1.0
+ */
+public class Board {
+  public static int width = 30;
+  public static int height = 20;
+     /**
+     * Constructor
+     *
+     * @param a adapter which get the window events
+     */
+  public Board () {
+
+  }
+}
